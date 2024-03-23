6e6e3c0e907c815423736f1387c187b965452365
Kirill Ryabov <sailor.kirill@gmail.com>
2024-03-23 22:09:11 +0700

    add client specific consts & types

---
+++ internal/domain/client.go
@@ -0,0 +1,60 @@
+package domain
+
+import (
+	"github.com/pkg/errors"
+)
+
+var (
+	ErrConnectionClosed = errors.New("connection closed")
+	ErrEmptyMessage     = errors.New("empty message")
+)
+
+type messageType byte
+
+const (
+	StartGame = messageType(iota)
+	RequestMove
+	PlayerMove
+)
+
+type Message struct {
+	Type    messageType
+	Payload any
+}
+
+type StartGamePayload struct {
+	CellType Cell
+	Board    Board
+}
+
+type PlayerMovePayload struct {
+	CellType        Cell
+	Position        byte
+	IsMoveRequested bool
+	GameResult      *string
+}
+
+type PlayerMovePayloadOption func(p *PlayerMovePayload)
+
+func RequestMoveBack() PlayerMovePayloadOption {
+	return func(p *PlayerMovePayload) {
+		p.IsMoveRequested = true
+	}
+}
+
+func WithGameResult(gameResultMsg string) PlayerMovePayloadOption {
+	return func(p *PlayerMovePayload) {
+		p.GameResult = &gameResultMsg
+	}
+}
+
+func WithCellType(cellType Cell) PlayerMovePayloadOption {
+	return func(p *PlayerMovePayload) {
+		p.CellType = cellType
+	}
+}
+
+type Client interface {
+	WriteMessage(msg Message) error
+	ReadMessage() (Message, error)
+}
