146224432ba006a618c0c7f14e96f992dbdf32bf
Kirill Ryabov <sailor.kirill@gmail.com>
2024-03-23 22:09:35 +0700

    add game specific consts & types

---
+++ internal/domain/game.go
@@ -0,0 +1,53 @@
+package domain
+
+import (
+	"context"
+)
+
+type Cell byte
+
+const (
+	None = Cell(' ')
+	X    = Cell('X')
+	O    = Cell('O')
+)
+
+type MoveStatus byte
+
+const (
+	NoneMove = MoveStatus(iota)
+	MoveX
+	MoveO
+	Draw
+	WinX
+	WinO
+)
+
+type Move struct {
+	CellType Cell
+	Position byte
+	Status   MoveStatus
+}
+
+type Board [9]Cell
+
+type status byte
+
+const (
+	ReadyToStart = status(iota)
+	InProgress
+	Finished
+)
+
+type GameState struct {
+	Board       Board
+	PlayerX     string
+	PlayerO     string
+	CurrentMove Cell
+	Status      status
+	Round       uint8
+}
+
+type GameUseCase interface {
+	Play(ctx context.Context, player Player, state *GameState) error
+}
